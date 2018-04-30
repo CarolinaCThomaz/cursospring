80b7139eb37988343f3ad09c7fd39f91249c826e
Carolina Thomaz <carol.com.jp@gmail.com>
2018-04-30 15:14:15 -0300

    Categoria teste

--- src/main/java/com/carolinachang/test/resources/CategoriaResource.java
+++ src/main/java/com/carolinachang/test/resources/CategoriaResource.java
@@ -2,6 +2,13 @@ package com.carolinachang.test.resources;
 
 public class CategoriaResource {
 
+	public int id;
+	
+	public CategoriaResource(int id) {
+		super();
+		this.id = id;
+	}
+
 	public CategoriaResource() {
 		super();
 		// TODO Auto-generated constructor stub
