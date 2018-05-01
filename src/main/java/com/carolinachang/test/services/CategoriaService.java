347e86711fd26f05cee24d7ccdae6d6b56e0623d
Carolina Thomaz <carol.com.jp@gmail.com>
2018-05-01 13:56:14 -0300

    Ajustes no endpoint /categorias/id - Tratamentos de erro

--- src/main/java/com/carolinachang/test/services/CategoriaService.java
+++ src/main/java/com/carolinachang/test/services/CategoriaService.java
@@ -2,11 +2,13 @@ package com.carolinachang.test.services;
 
 import java.util.Optional;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.carolinachang.test.domain.Categoria;
 import com.carolinachang.test.repositories.CategoriaRepository;
+import com.carolinachang.test.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +18,8 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repositorioCategoria.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName(), null));
 		
 	}
 }
