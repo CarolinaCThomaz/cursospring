e44dec59983d956f56d0a6c3eb842f137b8c7bae
Carolina Thomaz <carol.com.jp@gmail.com>
2018-05-05 10:01:13 -0300

    Ajustando e testando DELETE CLIENTE

--- src/main/java/com/carolinachang/test/services/ClienteService.java
+++ src/main/java/com/carolinachang/test/services/ClienteService.java
@@ -63,7 +63,7 @@ public class ClienteService {
 		try {
 			repositorioCliente.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityViolationException("Não é possível excluir porque há entidades relacionadas");
+			throw new DataIntegrityViolationException("Não é possível excluir porque há pedidos relacionados");
 		}
 		
 		
