9e687a06d93a4ca80315f2daa4b8bc08ef154e1d
Carolina Thomaz <carol.com.jp@gmail.com>
2018-05-10 23:40:06 -0300

    Amazon S3 - salvando primeira imagem

--- src/main/java/com/carolinachang/test/TestApplication.java
+++ src/main/java/com/carolinachang/test/TestApplication.java
@@ -1,12 +1,22 @@
 package com.carolinachang.test;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.carolinachang.test.services.S3Service;
+
 @SpringBootApplication
 public class TestApplication implements CommandLineRunner{
 
+	
+	private Logger LOG = LoggerFactory.getLogger(S3Service.class);
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	
 	public static void main(String[] args) {
@@ -15,7 +25,10 @@ public class TestApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-				
-		
+		try {	
+		s3Service.uploadFile("C:\\lagoa.jpg");
+		}catch (Exception e) {
+			LOG.info("Erro ao carregar img" +e.getMessage()) ;
+		}
 	}
 }
